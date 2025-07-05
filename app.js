e0a9752cb75479023a8cd79cb660e43386592ee8
moqtada2130 <moqtadav@gmail.com>
2025-07-05 18:32:07 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSy...YOUR_API_KEY...",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdefghij"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+console.log("Firebase initialized");
